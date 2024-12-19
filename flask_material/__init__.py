8b52a761d5782396b3f2f3b10168ea4178eab1f1
Leon <leon.m.heinen@gmail.com>
2024-12-19 18:28:12 +0100

    -minor changes

--- flask_material/__init__.py
+++ flask_material/__init__.py
@@ -1,8 +1,16 @@
-from flask import Flask
+import os
+from flask import Flask, Blueprint, send_from_directory
 from flask_material.pages import home
+
+root_path = os.path.dirname(__file__)
+bp = Blueprint("flask_material", __name__, static_folder = root_path + "\\static\\", template_folder = root_path + "\\templates\\", url_prefix = '/flask_material')
+
+def init(app: Flask):
+    app.register_blueprint(bp)
 
 def create_app(config = None):
     app = Flask(__name__)
+    init(app)
 
     # Try to load config
     if config is None:
